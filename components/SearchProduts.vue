3ee5b25a07832ed5e26f79f50fe1cac1654e27a7
istamovv <istamov.valeriy@wildberries.work>
2023-11-02 20:16:55 +0300

    search redesign

--- components/SearchProduts.vue
+++ components/SearchProduts.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="search-block"
-       @click="isActive = !isActive"
-  >
-    Поиск по сайту
-    <div v-if="isActive" class="search-hidden">
-      <input type="text"
-             v-focus
-             v-model="inputSearch"
-             @keyup.enter="searchProducts"
-             @click.stop>
-      <button class="enter"
-              @click="searchProducts">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 256 256">
-          <g fill="#808080">
-            <path d="M192 112a80 80 0 1 1-80-80a80 80 0 0 1 80 80Z" opacity=".2"/>
-            <path
-                d="m229.66 218.34l-50.06-50.06a88.21 88.21 0 1 0-11.32 11.31l50.06 50.07a8 8 0 0 0 11.32-11.32ZM40 112a72 72 0 1 1 72 72a72.08 72.08 0 0 1-72-72Z"/>
-          </g>
-        </svg>
-      </button>
-    </div>
+  <div class="search-block">
+    <Icon name="ph:magnifying-glass" size="20px"/>
+    <input type="text"
+           placeholder="Поиск по сайту"
+           v-model="inputSearch"
+           @keyup.enter="searchProducts"
+           @click.stop>
+    <button :class="[{visible : inputSearch !== ''}, 'enter']"
+            @click="searchProducts">
+      <Icon name="ph:arrow-right-light" size="20px"/>
+    </button>
   </div>
 </template>
 
@@ -30,15 +20,11 @@ const router = useRouter()
 const isActive = ref(false)
 const inputSearch = ref('')
 
-const vFocus = {
-  mounted: (el: HTMLInputElement) => el.focus()
-}
-
 const searchProducts = () => {
   if (inputSearch.value) {
     (inputSearch.value)
 
-    router.push(`search/ ${inputSearch.value}`)
+    router.push(`search/ ${ inputSearch.value }`)
     inputSearch.value = ''
     isActive.value = false
   }
@@ -53,27 +39,29 @@ const searchProducts = () => {
 
   border: 0
   height: 40px
+  padding: 5px
   cursor: pointer
   user-select: none
-  padding: 5px 15px
-  border-radius: 5px
   width: fit-content
-  background-color: var(--background-grey)
+  border-bottom: 1px solid var(--middle-grey)
   font-size: calc((100vw - 320px) / (1280 - 320) * (16 - 14) + 14px)
 
-  .search-hidden
-    display: flex
-    align-items: center
-    input
-      border: 0
-      outline: 0
-      color: var(--grey)
-      background-color: inherit
+  input
+    border: 0
+    outline: 0
+    max-width: 150px
+    color: var(--grey)
 
   .enter
-    border: none
-    margin: auto
-    display: flex
+    opacity: 0
     padding: 5px
+    border: none
+    display: flex
+    visibility: hidden
+    background-color: transparent
     font-size: calc((100vw - 320px) / (1280 - 320) * (14 - 12) + 12px)
+
+  .visible
+    opacity: 100%
+    visibility: visible
 </style>
